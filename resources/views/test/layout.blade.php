cecea216f14cd096dc0487d32d48e55744728a72
raidadada555-a11y <raidadada555@gmail.com>
2026-07-27 01:31:49 +0900

    7/27

--- resources/views/test/layout.blade.php
+++ resources/views/test/layout.blade.php
@@ -8,6 +8,18 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+    @auth('admin')
+        <div class="container mt-2">
+            <menu label="リンク">
+                管理画面機能 1<br>
+                管理画面機能 2<br>
+                管理画面機能 3<br>
+                管理画面機能 4<br>
+                <a href="/admin/logout">ログアウト</a><br>
+            </menu>
+        </div>
+    @endauth
+
     <div class="container mt-4">
         @yield('contents')
     </div>
