3cc2ae039a7e2a203aef59c1b5fc0376528b6e58
raidadada555-a11y <raidadada555@gmail.com>
2026-07-31 07:53:11 +0900

    Update completed_list.blade.php

--- resources/views/task/completed_list.blade.php
+++ resources/views/task/completed_list.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <h1>完了タスク一覧</h1>
-    
+
     <div class="mb-3">
         <a href="/task/list" class="btn btn-secondary">タスク一覧に戻る</a>
     </div>
@@ -14,24 +14,23 @@
                 <th>タスク名</th>
                 <th>期限</th>
                 <th>重要度</th>
-                <th>詳細</th>
+                <th>タスク詳細</th>
                 <th>タスク終了日</th>
             </tr>
         </thead>
         <tbody>
-            @foreach($completedTasks as $task)
-            <tr>
-                <td>{{ $task->name }}</td>
-                <td>{{ $task->period }}</td>
-                <td>{{ $task->getPriorityString() }}</td>
-                <td>{{ $task->detail }}</td>
-                <td>{{ $task->created_at }}</td>
-            </tr>
+            @foreach ($completedTasks as $task)
+                <tr>
+                    <td>{{ $task->name }}</td>
+                    <td>{{ $task->period }}</td>
+                    <td>{{ $task->getPriorityString() }}</td>
+                    <td>{{ $task->detail }}</td>
+                    <td>{{ $task->created_at }}</td>
+                </tr>
             @endforeach
         </tbody>
     </table>
 
-    {{-- ページネーションのリンク --}}
     {{ $completedTasks->links() }}
 </div>
 @endsection
